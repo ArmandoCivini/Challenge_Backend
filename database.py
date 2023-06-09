d0ed02dd0649f4ff38f1980cb30ca4e9f23f576a
Tomas Civini <tcivini@gmail.com>
2023-06-09 12:44:09 -0300

    change structure of language assosiation to accept an additional argument

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ Base = declarative_base()
 
 
 class Country(Base):
-    __tablename__ = "countries"
+    __tablename__ = "countries_table"
 
     name = Column(String, primary_key=True)
     capital = Column(String)
@@ -18,13 +18,29 @@ class Country(Base):
                               back_populates='countries'
                               )
     continent = Column(String)
-    languages = relationship('Language',
-                             secondary='country_language',
-                             back_populates='countries'
-                             )
+    languages = relationship("CountryLanguage", back_populates="country")
     population = Column(Integer)
     flag = Column(String)
     area = Column(Integer)
+
+
+class Language(Base):
+    __tablename__ = "languages_table"
+
+    name = Column(String, primary_key=True)
+    countries = relationship("CountryLanguage", back_populates="language")
+
+
+class CountryLanguage(Base):
+    __tablename__ = "country_language"
+
+    country_name = Column(String, ForeignKey('countries_table.name'),
+                          primary_key=True)
+    language_name = Column(String, ForeignKey('languages_table.name'),
+                           primary_key=True)
+    order = Column(Integer)
+    language = relationship("Language", back_populates="countries")
+    country = relationship("Country", back_populates="languages")
 
 
 class Currency(Base):
@@ -42,65 +58,20 @@ class Currency(Base):
 class CountryCurrency(Base):
     __tablename__ = "country_currency"
 
-    country_name = Column(String, ForeignKey('countries.name'),
+    country_name = Column(String, ForeignKey('countries_table.name'),
                           primary_key=True)
     currency_code = Column(String, ForeignKey('currencies.code'),
                            primary_key=True)
 
 
-class Language(Base):
-    __tablename__ = "languages"
-
-    name = Column(String, primary_key=True)
-    countries = relationship('Country',
-                             secondary='country_language',
-                             back_populates='languages'
-                             )
-
-
-class CountryLanguage(Base):
-    __tablename__ = "country_language"
-
-    country_name = Column(String, ForeignKey('countries.name'),
-                          primary_key=True)
-    language_name = Column(String, ForeignKey('languages.name'),
-                           primary_key=True)
-
 # Create the tables in the database
 Base.metadata.create_all(engine)
 
-# Test it
-with Session(bind=engine) as session:
+p = Country()
+a = CountryLanguage(order=1)
+a.language = Language()
+p.languages.append(a)
 
-    # add users
-    usr1 = Country(name="bob")
-    session.add(usr1)
-
-    usr2 = Country(name="alice")
-    session.add(usr2)
-
-    session.commit()
-
-    # add projects
-    prj1 = Language(name="Project 1")
-    session.add(prj1)
-
-    prj2 = Language(name="Project 2")
-    session.add(prj2)
-
-    session.commit()
-
-    # map users to projects
-    prj1.countries = [usr1]
-    prj1.countries.append(usr2)
-    prj2.countries = [usr2]
-
-    session.commit()
-
-
-with Session(bind=engine) as session:
-
-    print(session.query(Country).where(Country.name == "bob").one().languages[0].name)
-    print(session.query(Language).where(Language.name == "Project 2").one().countries)
-    stmt = select('*').select_from(text("country_language"))
-    print(session.execute(stmt).fetchall())
+for assoc in p.languages:
+    print(assoc.order)
+    print(assoc.language)
